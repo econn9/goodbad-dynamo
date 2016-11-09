342c5f9dcf839de516b4e99db1711112e4fc02f7
econnor <econnor@cars.com>
2016-11-08 22:51:30 -0600

    initial

--- server.js
+++ server.js
@@ -1,60 +1,30 @@
 (function () {
     'use strict';
-    let AWS = require('aws-sdk');
 
-    AWS.config.update({
-        region: "us-east-2",
-        endpoint: "http://localhost:8010"
+    let path = require('path');
+    let mongoose = require('mongoose');
+    let bodyParser = require('body-parser');
+    let express = require('express');
+    let Rx = require('rx');
+
+    let app = express();
+
+    mongoose.connect('mongodb://localhost/goodbad');
+
+    let today = mongoose.model('today', {
+        good: String,
+        goodLink: String,
+        goodYear: Number,
+        bad: String,
+        badLink: String,
+        badYear: Number,
+        day: Number,
+        month: Number
     });
-
-    ///////////////////////////
-
-    let dynamo = new AWS.DynamoDB();
-    let docClient = new AWS.DynamoDB.DocumentClient();
-
-    let path = require('path'),
-        bodyParser = require('body-parser'),
-        express = require('express'),
-        app = express();
-
-    let params = {
-        TableName : "test1",
-        KeySchema: [
-            { AttributeName: "daymonth", KeyType: "HASH"},  //Partition key
-        ],
-        AttributeDefinitions: [
-            { AttributeName: "daymonth", AttributeType: "S" }
-        ],
-        ProvisionedThroughput: {
-            ReadCapacityUnits: 10,
-            WriteCapacityUnits: 10
-        }
-    };
-
-    dynamo.describeTable({TableName: params.TableName}, function (err, data) {
-        if (err) {
-               console.error("***Red Alert*** describeTable failed. Message: ", JSON.stringify(err, null, 2));
-           } else {
-               console.log(params.TableName+ " table created. JSON: ", JSON.stringify(data, null, 2));
-               if (!data.Table.TableStatus == "ACTIVE"){
-                   createDynamoTable(params);
-               }
-           }
-    });
-
-
-    function createDynamoTable(params){
-        dynamo.createTable(params, function(err, data) {
-            if (err) {
-                console.error("***Red Alert*** createTable failed. Message:", JSON.stringify(err, null, 2));
-            } else {
-                console.log("Successfully created table: ", JSON.stringify(data, null, 2));
-            }
-        });
-    }
-
     app.use("/css", express.static(__dirname + '/css'));
     app.use("/js", express.static(__dirname + '/js'));
+
+
     app.use(bodyParser.urlencoded({extended: true}));
 
     app.get('/', function (request, response) {
@@ -65,24 +35,21 @@
         response.sendFile(path.resolve('input.html'));
     });
 
-    app.get('/api/event/:daymonth', function (request, response) {
-        let key = request.params.daymonth;
+    app.get('/api/event/:month/:day', function (request, response) {
+        let month = request.params.month;
+        let day = request.params.day;
 
-        let getParams = {
-            TableName:"test1",
-            Key: {
-                daymonth: key
+        today.find({'month': month, 'day': day}, function(err, events) {
+            if(err) {
+                response.json({
+                    error: err
+                });
+                return;
             }
-        }
 
-        docClient.get(getParams, function(err, data) {
-            if (err) {
-                console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
-            } else {
-                console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
-                response.json(data);
-            }
+            response.json(events);
         });
+
     });
 
     app.get('api/events', function (request, response) {
@@ -91,32 +58,30 @@
         });
     });
 
-    // app.post('/api/setDate', function (req, res) {
-    //     console.log("+++++++++ ", req);
-    // });
-
     app.post('/api/event', function(request, response) {
-        let daymonthVal = '' + groomDateInt(request.body.month) + groomDateInt(request.body.day);
-        let entry = {
-            TableName:"test1",
-            Item: {
-                "daymonth": daymonthVal, //must be a string
-                "bad": request.body.bad,
-                "badLink": request.body.badLink,
-                "badYear": request.body.badYear,
-                "good": request.body.good,
-                "goodLink": request.body.goodLink,
-                "goodYear": request.body.goodYear
-            }
-        }
+        console.log('body is >>> ' + request.body);
 
-        docClient.put(entry, function(err, data) {
-            if (err) {
-                console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
-            } else {
-                console.log("Added item:", JSON.stringify(data, null, 2));
-                response.json(data);
+        let insert = new today({
+            month: request.body.month,
+            day: request.body.day,
+            good: request.body.good,
+            goodLink: request.body.goodLink,
+            goodYear: request.body.goodYear,
+            bad: request.body.bad,
+            badLink: request.body.badLink,
+            badYear: request.body.badYear
+        });
+
+        insert.save(function(err, event) {
+            console.log('body is >>> ' + request.body);
+
+            if(err) {
+                response.json({
+                    error: err
+                });
+                return;
             }
+            response.json(event);
         });
     });
 
@@ -138,20 +103,9 @@
         });
     });
 
-    app.listen(8002, function(){
+    app.listen(9000, function(){
         let time = new Date();
-        console.log('we up...   -_______- ', time);
+        console.log('we up on 80 ma nigga', time);
     });
-
-    function groomDateInt(val){
-        if (val.indexOf("0") == 0){
-            val.replace(0, '');
-        }
-        if (val > 0 && val < 10){
-            return '0'+val;
-        } else {
-            return ''+val;
-        }
-    }
 
 })();
